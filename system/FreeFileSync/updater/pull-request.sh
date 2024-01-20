b5687a0b61c0c46035df842ce1988bdea2bbddc0
SmurfDaddy <antonioleal@yahoo.com>
2024-01-20 13:22:49 +0000

    new pull-request script

--- system/FreeFileSync/updater/pull-request.sh
+++ system/FreeFileSync/updater/pull-request.sh
@@ -3,6 +3,9 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd $SCRIPT_DIR
 
+set -e
+clear
+
 cd ..
 PKGNAME=${PWD##*/}
 echo "Package: " $PKGNAME
@@ -11,12 +14,47 @@ CATEGORY=${PWD##*/}
 echo "Category: " $CATEGORY
 
 read -p "Commit message: " MSG
+echo
+echo
 
-cd ~/slackware-builds/antonioleal/slackbuilds/$CATEGORY/
+cd ~/slackware-builds/antonioleal/slackbuilds
 git checkout -B $PKGNAME
-tar xvfz ~/slackware-builds/antonioleal/myslackbuilds/$CATEGORY/$PKGNAME/updater/slackbuild/$PKGNAME.tar.gz
-git commit -a -m $MSG
-git push -f origin $PKGNAME
-git checkout master
-git branch -D $PKG
+cd ~/slackware-builds/antonioleal/slackbuilds/$CATEGORY/
+echo
+echo
 
+tar xvfz ~/slackware-builds/antonioleal/myslackbuilds/$CATEGORY/$PKGNAME/updater/slackbuild/$PKGNAME.tar.gz
+echo
+echo
+
+# spawn pull-request console and show follow-up commands (I prefer to do this manually)
+#konsole -e /bin/bash --rcfile <(echo "cd ~/slackware-builds/antonioleal/slackbuilds/$CATEGORY/$PKGNAME ; pwd ; echo ; ls --color ; echo ; git status") &
+
+pwd
+git status
+
+# git commit -a -m "$CATEGORY/$PKGNAME: $MSG"
+echo
+echo git commit -a -m "\"$CATEGORY/$PKGNAME: $MSG\""
+read -p "Issue command above? (y/n) " op
+if [ "$op" = "y" ] || [ "$op" = "Y" ]; then
+    git commit -a -m "$CATEGORY/$PKGNAME: $MSG"
+fi
+
+# git push -f origin $PKGNAME
+echo
+echo git push -f origin $PKGNAME
+read -p "Issue command above? (y/n) " op
+if [ "$op" = "y" ] || [ "$op" = "Y" ]; then
+    git push -f origin $PKGNAME
+fi
+
+#   or delete all branches
+echo
+echo 'git branch | grep -v "master" | xargs git branch -D'
+read -p "Issue command above? (y/n) " op
+if [ "$op" = "y" ] || [ "$op" = "Y" ]; then
+    git checkout master
+    #git branch -D $PKG
+    git branch | grep -v "master" | xargs git branch -D
+fi
