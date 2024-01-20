982246dfb20d1a54cdc9a07d798aa307c09b39c3
SmurfDaddy <antonioleal@yahoo.com>
2024-01-20 13:06:20 +0000

    updates

--- system/timeshift/updater/pull-request.sh
+++ system/timeshift/updater/pull-request.sh
@@ -3,6 +3,8 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd $SCRIPT_DIR
 
+set -e
+
 cd ..
 PKGNAME=${PWD##*/}
 echo "Package: " $PKGNAME
@@ -11,17 +13,41 @@ CATEGORY=${PWD##*/}
 echo "Category: " $CATEGORY
 
 read -p "Commit message: " MSG
+echo
+echo
 
-cd ~/slackware-builds/antonioleal/slackbuilds/$CATEGORY/
-
+cd ~/slackware-builds/antonioleal/slackbuilds
 git checkout -B $PKGNAME
-sleep 2
+cd ~/slackware-builds/antonioleal/slackbuilds/$CATEGORY/
+echo
+echo
 
 tar xvfz ~/slackware-builds/antonioleal/myslackbuilds/$CATEGORY/$PKGNAME/updater/slackbuild/$PKGNAME.tar.gz
-sleep 2
+echo
+echo
 
-echo git commit -a -m "$CATEGORY/$PKGNAME: $MSG"
+# spawn pull-request console and show follow-up commands (I prefer to do this manually)
+#konsole -e /bin/bash --rcfile <(echo "cd ~/slackware-builds/antonioleal/slackbuilds/$CATEGORY/$PKGNAME ; pwd ; echo ; ls --color ; echo ; git status") &
+
+pwd
+git status
+
+# git commit -a -m "$CATEGORY/$PKGNAME: $MSG"
+echo
+echo git commit -a -m "\"$CATEGORY/$PKGNAME: $MSG\""
+read -p "Issue command above? (y/n) " op
+if [ "$op" = "y" ] || [ "$op" = "Y" ]; then
+    echo "BOMBA"
+    #git commit -a -m "$CATEGORY/$PKGNAME: $MSG"
+fi
+
 echo git push -f origin $PKGNAME
 echo git checkout master
-echo git branch -D $PKG
 
+#echo git branch -D $PKG
+# delete all branches
+#git branch | grep -v "master" | xargs git branch -D
+echo 'git branch | grep -v "master" | xargs git branch -D'
+
+
+# manual clean-up: git checkout master && git branch -D timeshift && git reset --hard && git branch && git status
