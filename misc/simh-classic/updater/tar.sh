7dfc314ce82e9ab33db62eb0d2c0ad3d30535842
SmurfDaddy <antonioleal@yahoo.com>
2024-02-17 12:31:17 +0000

    simh-classic updater

---
+++ misc/simh-classic/updater/tar.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd ..
+sboname=${PWD##*/}
+echo $sboname
+cd updater/slackbuild
+
+tar -z -c -f $sboname.tar.gz -v --exclude='updater' --exclude='*.tar.gz' --exclude='*.sf3' --exclude='*.zip'  ../../../$sboname
