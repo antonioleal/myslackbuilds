b448296b588b4ba56ff8fe99b4a89cc0728f3dd4
Antonio Leal <antonio.leal@yahoo.com>
2024-03-01 20:31:19 +0000

    new tar.sh

--- multimedia/MuseScore-Legacy/updater/tar.sh
+++ multimedia/MuseScore-Legacy/updater/tar.sh
@@ -5,4 +5,13 @@ sboname=${PWD##*/}
 echo $sboname
 cd updater/slackbuild
 
-tar -z -c -f $sboname.tar.gz -v --exclude='updater' --exclude='*.tar.gz' --exclude='*.sf3' --exclude='*.md'  ../../../$sboname
+tar -z -c -f $sboname.tar.gz -v \
+    --exclude='updater'     \
+    --exclude='*.tar.gz'    \
+    --exclude='*.tar'       \
+    --exclude='*.zip'       \
+    --exclude='*.md'        \
+    --exclude='*.sf3'       \
+    --exclude='*.deb'       \
+    --exclude='*.rpm'       \
+    ../../../$sboname
