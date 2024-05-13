a1ab3ac5948556a4847008a54e69531d38324af0
SmurfDaddy <antonioleal@yahoo.com>
2024-05-13 21:32:40 +0100

    corrected open-simh

--- misc/open-simh/updater/updater.sh
+++ misc/open-simh/updater/updater.sh
@@ -67,8 +67,8 @@ mv $TARBALL ..
 ################################
 MD5=`md5sum ../$TARBALL | cut -d " " -f 1`
 DATEVERSION=`tar tvfz ../$TARBALL | head -n1 | awk '{ print $4 }' | awk 'BEGIN { FS = "-" } ; { print $1$2$3 }'`
-sed -e "s/\${_version_}/${DATEVERSION}_${NEWVERSION}/" -e "s/\${_commit_}/$COMMIT/" -e "s/\${_md5_}/$MD5/" $SCRIPT_DIR/template/${PRGNAM}.info.template > ../${PRGNAM}.info
-sed -e "s/\${_version_}/${DATEVERSION}_${NEWVERSION}/" -e "s/\${_commit_}/$COMMIT/" $SCRIPT_DIR/template/${PRGNAM}.SlackBuild.template > ../${PRGNAM}.SlackBuild
+sed -e "s/\${_version_}/${NEWVERSION}/" -e "s/\${_fullversion_}/${DATEVERSION}_${NEWVERSION}/" -e "s/\${_commit_}/$COMMIT/" -e "s/\${_md5_}/$MD5/" $SCRIPT_DIR/template/${PRGNAM}.info.template > ../${PRGNAM}.info
+sed -e "s/\${_version_}/${NEWVERSION}/" -e "s/\${_fullversion_}/${DATEVERSION}_${NEWVERSION}/" -e "s/\${_commit_}/$COMMIT/" $SCRIPT_DIR/template/${PRGNAM}.SlackBuild.template > ../${PRGNAM}.SlackBuild
 chmod +x ../${PRGNAM}.SlackBuild
 
 ################################
