af780bde5dff36d8b5565f2332bd99e9173063aa
Antonio Leal <antonio.leal@yahoo.com>
2024-07-27 13:36:39 +0100

    b2 b2-20240720-010024-9f95270

--- system/b2/0/clone.sh
+++ system/b2/0/clone.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-TAG=b2-20240710-232859-844adac
+TAG=b2-20240720-010024-9f95270
+echo
+echo "cloning tag $TAG"
+echo
 
 git clone --recursive --branch $TAG https://github.com/tom-seddon/b2.git
 rm -rf b2/.git
