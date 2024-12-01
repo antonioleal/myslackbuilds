3e669d16ced29d5d21ee34a7c087fa5fd96bab28
Antonio Leal <antonio.leal@yahoo.com>
2024-12-01 15:35:44 +0000

    new 0 menu

---
+++ 0/0.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+# Slackware menu for 0scripts
+
+# Copyright 2024 Antonio Leal, Porto Salvo, Oeiras, Portugal
+# All rights reserved.
+#
+# Redistribution and use of this script, with or without modification, is
+# permitted provided that the following conditions are met:
+#
+# 1. Redistributions of this script must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR IMPLIED
+#  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+#  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO
+#  EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+#  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+#  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+#  OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+#  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+#  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+#  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+PKGNAME=${PWD##*/}
+HEIGHT=18
+WIDTH=88
+CHOICE_HEIGHT=18
+BACKTITLE="Antonio Leal's \"0\" scripts SlackBuild environment: $PKGNAME"
+TITLE="WORKFLOW"
+MENU="Choose one of the following options for $PKGNAME:"
+
+OPTIONS=(1 "less $PKGNAME.info"
+         2 "0meld.sh                        # check if SlackBuilds.org changed the script"
+         3 "0replace-string.sh              # changes a string in *.* using sed"
+         4 "0download-source-tarballs.sh    # get the source code"
+         5 "0update-md5-info.sh             # recalculates md5 and updates info file"
+         6 "0build.sh                       # builds the package"
+         7 "0make-readme.sh"
+         8 "0commit-push.sh"
+         9 "0pull-request.sh"
+         0 "0clean-tree.sh"
+         x "exit"
+         )
+
+while (true)
+do
+    CHOICE=$(dialog --clear \
+                    --backtitle "$BACKTITLE" \
+                    --title "$TITLE" \
+                    --menu "$MENU" \
+                    $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                    "${OPTIONS[@]}" \
+                    2>&1 >/dev/tty)
+
+    clear
+    case $CHOICE in
+            1)
+                less $PKGNAME.info
+                ;;
+            2)
+                0meld.sh
+                ;;
+            3)
+                0replace-string.sh
+                read -p "Press [ENTER] to continue." op
+                ;;
+            4)
+                0download-source-tarballs.sh
+                ;;
+            5)
+                0update-md5-info.sh
+                ;;
+            6)
+                0build.sh
+                ;;
+            7)
+                0make-readme.sh
+                ;;
+            8)
+                0commit-push.sh
+                ;;
+            9)
+                0pull-request.sh
+                ;;
+
+            0)
+                0clean-tree.sh
+                ;;
+
+            x)
+                exit 0
+                ;;
+    esac
+done
+
