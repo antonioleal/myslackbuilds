7bdaeebb3d20ed55cff7a2b2999886830cdac6fb
Antonio Leal <antonio.leal@yahoo.com>
2025-05-02 14:58:00 +0100

    smoffice2021 updated for version 2021_1068

--- office/smoffice2021/0/updater.sh
+++ office/smoffice2021/0/updater.sh
@@ -2,7 +2,7 @@
 
 # Slackware updater script for smoffice2021
 
-# Copyright 2023 Antonio Leal, Porto Salvo, Oeiras, Portugal
+# Copyright 2023-2025 Antonio Leal, Porto Salvo, Oeiras, Portugal
 # All rights reserved.
 #
 # Redistribution and use of this script, with or without modification, is
@@ -30,89 +30,41 @@ cd $SCRIPT_DIR
 ################################
 # check versions               #
 ################################
-# OLDVERSION
-touch old_version
-OLDVERSION=`cat old_version`
-echo "Old version is $OLDVERSION"
-
-# NEWVERSION
 #curl -s -o index.html http://valentina-db.com/download/prev_releases/?C=M;O=A
 curl -s -o index.html https://www.softmaker.de/support/installation/linux/office-2021
 #NEWVERSION=`lynx -dump index.html | tail -1 | cut -d "/" -f 14`
 NEWVERSION=`lynx -dump index.html  | grep "2021.*amd64.tgz" | cut -d "-" -f4 | tail -n1`
+rm -rf index.html
+TARBALL=softmaker-office-2021-${NEWVERSION}-amd64.tgz
+URL="https://www.softmaker.net/down/${TARBALL}"
+#URL="https://www.softmaker.net/down/softmaker-office-2021-1068-amd64.tgz"
 
-
-echo "New version is $NEWVERSION"
-echo $NEWVERSION > new_version
-
-if [ "$OLDVERSION" = "$NEWVERSION" ]; then
-    echo "No new version detected..."
-    return
-fi
-
-################################
-# download tarball             #
-################################
-cd ..
-set +e
-rm *.tgz
-set -e
-TARBALL=softmaker-office-2021-$NEWVERSION-amd64.tgz
-wget https://www.softmaker.net/down/$TARBALL
-if [ ! -f ./$TARBALL ]
+VERSION=`cat version`
+if [ "$VERSION" = "$NEWVERSION" ]
 then
-    echo "File $TARBALL not found, aborting..."
-    exit 1
+    echo "updater.sh says $PRGNAM is already at version $VERSION. No new update."
+else
+    ################################
+    # download tarball             #
+    ################################
+    wget $URL
+    if [ ! -f ./$TARBALL ]
+    then
+        echo "File $TARBALL not found, aborting..."
+        exit
+    fi
+    # delete old tarball and place new one
+    rm -rf ../*.tgz 2> /dev/null
+    mv *.tgz ..
+
+    ################################
+    # write templates              #
+    ################################
+    MD5=`md5sum ../$TARBALL | cut -d " " -f 1`
+    #DATEVERSION=`tar tvfz ../$TARBALL | head -n1 | awk '{ print $4 }' | awk 'BEGIN { FS = "-" } ; { print $1$2$3 }'`
+    sed -e "s/_version_/${NEWVERSION}/g" -e "s/_md5_/${MD5}/g" $SCRIPT_DIR/template/${PRGNAM}.info.template > ../${PRGNAM}.info
+    sed -e "s/_version_/${NEWVERSION}/g" $SCRIPT_DIR/template/${PRGNAM}.SlackBuild.template > ../${PRGNAM}.SlackBuild
+    chmod 644 ../${PRGNAM}.SlackBuild
+    echo "$NEWVERSION" > version
+    echo "updater.sh says $PRGNAM has a new version $NEWVERSION"
 fi
-
-################################
-# write templates              #
-################################
-MD5=`md5sum $TARBALL | cut -d " " -f 1`
-sed -e "s/\${_version_}/$NEWVERSION/" -e "s/\${_md5_}/$MD5/" $SCRIPT_DIR/template/${PRGNAM}.info.template > ${PRGNAM}.info
-sed -e "s/\${_version_}/$NEWVERSION/" $SCRIPT_DIR/template/${PRGNAM}.SlackBuild.template > ${PRGNAM}.SlackBuild
-chmod +x ${PRGNAM}.SlackBuild
-
-################################
-# build                        #
-################################
-set -e
-sudo ./${PRGNAM}.SlackBuild
-if [ ! -f /tmp/${PRGNAM}-2021_$NEWVERSION-x86_64-1_SBo.tgz ]
-then
-    echo "Build process not successfull, aborting..."
-    exit 1
-fi
-
-################################
-# make slackbuild tar.gz       #
-################################
-echo "Compressing SlackBuild release in slackbuild folder"
-cd $SCRIPT_DIR/../..
-rm -rf $SCRIPT_DIR/slackbuild/*
-tar -z -v -c -f $SCRIPT_DIR/slackbuild/${PRGNAM}.tar.gz --exclude='*.tgz' --exclude='updater' ${PRGNAM}
-
-################################
-# cleanup                      #
-################################
-cd $SCRIPT_DIR
-rm index.html
-rm new_version
-
-################################
-# sbopkglint                   #
-################################
-sbopkglint /tmp/${PRGNAM}-2021_$NEWVERSION-x86_64-1_SBo.tgz
-
-################################
-# make slackbuild tar.gz       #
-################################
-read -p "Proceed with install of /tmp/${PRGNAM}-2021_$NEWVERSION-x86_64-1_SBo.tgz ? (y/n) " RESP
-if [ ! "$RESP" = "y" ]; then
-    echo "bye"
-    exit
-fi
-sudo /sbin/upgradepkg --install-new /tmp/${PRGNAM}-2021_$NEWVERSION-x86_64-1_SBo.tgz
-echo $NEWVERSION > $SCRIPT_DIR/old_version
-echo
-echo "/tmp/${PRGNAM}-2021_$NEWVERSION-x86_64-1_SBo.tgz installed."
