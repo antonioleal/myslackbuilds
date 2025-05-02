322003753101bb51bd8653f674a507dee8f29be1
Antonio Leal <antonio.leal@yahoo.com>
2025-05-02 19:47:59 +0100

    waterfox updated for version 6.5.7

---
+++ network/waterfox/0/updater.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+# Slackware updater script for waterfox
+
+# Copyright 2025 Antonio Leal, Porto Salvo, Oeiras, Portugal
+# All rights reserved.
+#
+# Redistribution and use of this script, with or without modification, is
+# permitted provided that the following conditions are met:
+#
+# 1. Redistributions of this script must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR IMPLIED
+#  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+#  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO
+#  EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+#  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+#  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+#  OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+#  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+#  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+#  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+PRGNAM=waterfox
+set -e
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cd $SCRIPT_DIR
+
+################################
+# check versions               #
+################################
+
+#tag=$(curl -s https://api.github.com/repos/Alex313031/thorium/releases/latest | jq -r '.tag_name')
+#echo $tag
+
+# NEWVERSION
+#COMMIT=`git ls-remote https://github.com/stardot/b-em/ | head -1 | cut  -f 1`
+#NEWVERSION=${COMMIT:0:7}
+
+#COMMIT=`git ls-remote https://github.com/stardot/b-em/ | head -1 | cut  -f 1`
+#NEWVERSION=${COMMIT:0:7}
+
+#NEWVERSION=`curl -qsL "https://sourceforge.net/projects/doublecmd/best_release.json" | jq -r ".platform_releases.linux.filename" | cut -d "/" -f 3 | cut -d " " -f 3`
+#TARBALL=doublecmd-${NEWVERSION}-src.tar.gz
+
+#TAG=$(curl -s https://api.github.com/repos/wxFormBuilder/wxFormBuilder/releases/latest | jq -r '.tag_name')
+#NEWVERSION=${TAG:1}
+#TARBALL=wxFormBuilder-${NEWVERSION}-source-full.tar.gz
+
+#NEWVERSION=$(curl -s https://2484.de/yabasic/content_whatsnew.html | grep Version | head -n1 | cut -d " " -f 6 | cut -d "," -f 1)
+#TARBALL=yabasic-${NEWVERSION}.tar.gz
+
+NEWVERSION=$(curl -s https://api.github.com/repos/BrowserWorks/Waterfox/releases/latest | jq -r '.tag_name')
+TARBALL=waterfox-${NEWVERSION}.tar.bz2
+URL="https://cdn1.waterfox.net/waterfox/releases/${NEWVERSION}/Linux_x86_64/waterfox-${NEWVERSION}.tar.bz2"
+
+VERSION=`cat version`
+if [ "$VERSION" = "$NEWVERSION" ]
+then
+    echo "updater.sh says $PRGNAM is already at version $VERSION. No new update."
+    export RET0=""
+else
+    ################################
+    # download tarball             #
+    ################################
+    wget $URL
+    if [ ! -f ./$TARBALL ]
+    then
+        echo "File $TARBALL not found, aborting..."
+        exit
+    fi
+    # delete old tarball and place new one
+    rm -rf ../*.bz2 2> /dev/null
+    mv *.bz2 ..
+
+    ################################
+    # write templates              #
+    ################################
+    MD5=`md5sum ../$TARBALL | cut -d " " -f 1`
+    #DATEVERSION=`tar tvfz ../$TARBALL | head -n1 | awk '{ print $4 }' | awk 'BEGIN { FS = "-" } ; { print $1$2$3 }'`
+    sed -e "s/_version_/${NEWVERSION}/g" -e "s/_md5_/$MD5/g" $SCRIPT_DIR/template/${PRGNAM}.info.template > ../${PRGNAM}.info
+    sed -e "s/_version_/${NEWVERSION}/g" $SCRIPT_DIR/template/${PRGNAM}.SlackBuild.template > ../${PRGNAM}.SlackBuild
+    chmod 644 ../${PRGNAM}.SlackBuild
+    echo "$NEWVERSION" > version
+    echo "updater.sh says $PRGNAM has a new version $NEWVERSION"
+    export RET0=$NEWVERSION
+fi
