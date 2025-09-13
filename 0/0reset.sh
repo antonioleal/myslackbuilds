9f2b1ba196b6955a952df1273aa53633d1124439
Antonio Leal <antonio.leal@yahoo.com>
2025-09-13 10:37:24 +0100

    0reset.sh

--- 0/0reset.sh
+++ 0/0reset.sh
@@ -22,8 +22,7 @@
 #  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 #  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#set -e
-clear
+set -e
 
 if [ "$1" == "sl" ]; then
     cd ~/slackware-builds/slackbuilds
