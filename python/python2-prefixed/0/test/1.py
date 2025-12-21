352465613256c02ad7c9efbef493fb47e7ff99d0
Antonio Leal <antonio.leal@yahoo.com>
2025-12-21 16:57:12 +0000

    python2-prefixed updated for version 0.9.0

---
+++ python/python2-prefixed/0/test/1.py
@@ -0,0 +1,9 @@
+from prefixed import Float
+
+
+#print "print '{:.2h}s'.format(Float(.00001534))"
+print '{:.2h}s'.format(Float(.00001534))
+
+#print "print '{:.2k}B'.format(Float(42467328))"
+print '{:.2k}B'.format(Float(42467328))
+
