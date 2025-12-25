156c02448a096a7d4821993f65266d673b82f9e3
Antonio Leal <antonio.leal@yahoo.com>
2025-12-25 22:34:20 +0000

    python3-gmpy2 updated for version 2.2.2

--- README.md
+++ README.md
@@ -416,7 +416,7 @@ Instead of github go to Ardour.org and press download to get the source code tar
   
 
 - Category: python
-- Version: 2.2.1
+- Version: 2.2.2
 - Homepage: https://pypi.org/project/gmpy2/
 - Code repository: https://github.com/aleaxit/gmpy
 
---
+++ python/python3-gmpy2/0/test/1.py
@@ -0,0 +1,31 @@
+from gmpy2 import *
+
+print(mpz(99) * 43)
+print(pow(mpz(99), 37, 59))
+print(isqrt(99))
+print(isqrt_rem(99))
+print(gcd(123, 27))
+print(lcm(123, 27))
+print((mpz(123) + 12) / 5)
+print((mpz(123) + 12) // 5)
+print((mpz(123) + 12) / 5.0)
+print(mpz('123') + 1)
+print(10 - mpz(1))
+print(is_prime(17))
+print(mpz('1_000_000'))
+print(mpq(3, 7)/7)
+print(mpq(45, 3) * mpq(11, 8))
+print(mpq(1, 7) * 11)
+
+
+ctx = get_context()
+print(ctx.precision)
+ctx.precision = 100
+print(mpfr('1.2'))
+print(mpfr(float('1.2')))
+
+ctx.precision = 53
+ctx.round = RoundUp
+print(const_pi())
+ctx.round = RoundToNearest
+print(const_pi())
