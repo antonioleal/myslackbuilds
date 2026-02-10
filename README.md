865d946da458956638b757e9122caab441b16beb
Antonio Leal <antonioleal@yahoo.com>
2026-02-10 22:12:38 +0000

    python3-odfdo updated for version 3.20.2

--- README.md
+++ README.md
@@ -1466,7 +1466,7 @@ Instead of github go to Ardour.org and press download to get the source code tar
   
 
 - Category: python
-- Version: 3.20.0
+- Version: 3.20.2
 - Homepage: https://github.com/jdum/odfdo
 - Code repository: 
 ## python3-zstandard (Python3 bindings for Zstandard library)
--- python/python3-odfdo/0/test/1.sh
+++ python/python3-odfdo/0/test/1.sh
@@ -1,10 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env python3
 
-python3 1.py
+from odfdo import Document, Paragraph
 
-( /usr/bin/libreoffice ./hello.odt & )
-( /usr/bin/libreoffice ./modified_spreadsheet.ods & )
-sleep 10
-rm hello.odt
-rm modified_spreadsheet.ods
+# Writer test
+doc = Document('text')
+doc.body.append(Paragraph("Hello world!"))
+doc.save("hello.odt")
 
+# Calc test
+doc = Document('existing_spreadsheet.ods')
+sheet = doc.body.get_table(0)
+print(f"Value of A1: {sheet.get_cell('A1').value}")
+sheet.set_value('B2', 'Updated Cell Value by odfdo')
+doc.save('modified_spreadsheet.ods')
